54fd4bd32e2483de5c85cbbe07042d1f18d05b73
sehannnnnnn <stanley710@naver.com>
2022-03-05 14:50:00 +0900

    initialize Spring Boot Settings

---
+++ circlesearch/src/main/java/com/capstone/circlesearch/CirclesearchApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.circlesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CirclesearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CirclesearchApplication.class, args);
+	}
+
+}
---
+++ circlesearch/src/test/java/com/capstone/circlesearch/CirclesearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.circlesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CirclesearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
